786a766b86e22e36dffb464bc04f650773eefa46
vargheseibm <vargheseibm@gmail.com>
2023-03-29 15:22:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from forms import CreatePostForm, RegisterForm,LoginForm,CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
