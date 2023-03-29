688dea613ae27aac77433f0b5be8f84ad72d06de
vargheseibm <vargheseibm@gmail.com>
2023-03-29 15:28:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 from flask import abort
 import os
-
+import psycopg2
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
